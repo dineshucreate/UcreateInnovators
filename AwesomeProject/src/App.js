3926b7f86f9c0cf52ee3f990a7e929933894ed6b
shivam-ucreate <shivam@ucreate.co.in>
2019-04-05 18:42:16 +0530

    Safe Area, Flex grow, alignItems, justifyContent, flexDirection

--- AwesomeProject/src/App.js
+++ AwesomeProject/src/App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,7 +17,7 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
-  
+
   callback() {
     let calc = (num1, num2, callback) => {
       return callback(num1, num2);
@@ -33,14 +33,14 @@ export default class App extends Component {
   }
 
   promise() {
-    let prom = new Promise(function(resolve, reject) {
+    let prom = new Promise(function (resolve, reject) {
       let isClean = true;
       if (isClean == true) {
         resolve('Clean');
       } else {
         reject('Not cleaned');
       }
-    }); 
+    });
     prom.then((resolve) => {
       console.log('My room is ' + resolve);
     }).catch((err) => {
@@ -50,77 +50,73 @@ export default class App extends Component {
 
   // Complex promise
   complexPromise() {
-    let cleanRoom = function() {
-      return new Promise(function(resolve, reject) {
+    let cleanRoom = () => {
+      return new Promise((resolve, reject) => {
         resolve('Cleaned room ');
       });
     }
 
-    let removeGarbage = function(message) {
-      return new Promise(function(resolve, reject) {
-        resolve( message + 'Garbage collected ');
+    let removeGarbage = (message) => {
+      return new Promise((resolve, reject) => {
+        resolve(message + 'Garbage collected ');
       });
     }
-    let winIceCream = function(message) {
-      return new Promise(function(resolve, reject) {
+    let winIceCream = (message) => {
+      return new Promise(function (resolve, reject) {
         resolve(message + 'Ice cream ');
       });
     }
 
-    cleanRoom().then(function(resultRoom) {
+    cleanRoom().then((resultRoom) => {
       return removeGarbage(resultRoom);
-    }).then(function(resultRoom){
+    }).then((resultRoom) => {
       return winIceCream(resultRoom);
-    }).then(function(resultRoom){
+    }).then((resultRoom) => {
       console.log('Everything is finished ' + resultRoom);
     })
 
-    Promise.all([cleanRoom(), removeGarbage(), winIceCream()]).then(function(){
+    Promise.all([cleanRoom(), removeGarbage(), winIceCream()]).then(() => {
       console.log('Finished at once');
     })
 
-    Promise.race([cleanRoom(), removeGarbage(), winIceCream()]).then(function(){
+    Promise.race([cleanRoom(), removeGarbage(), winIceCream()]).then(() => {
       console.log('Call only one of them');
     })
 
     //Async/Await
-  
-      const preMovie = async () => {
-        const wifeBringingTickets = new Promise((resolve, reject) => {
-          setTimeout(() => {
-            resolve('tickets');
-          }, 3000);
-        })
-  
-        let ticket = await wifeBringingTickets;
-  
-        console.log(`Wife: I have the tickets ${tickets}` );
-        console.log(`Husband: we should go in`);
-        console.log(`Wife: No i need popcorn`);
-  
-        const getPopCorn = new Promise((resolve, reject) => {
-          resolve('pocorn');
-        })
-        let popcorn = await getPopCorn;
-        console.log(`Husband: we got popcorn ${popcorn}`);
-        console.log(`Husband: Now we should go in`);
-        console.log(`Wife: No i need butter`);
-  
-        const getButter = new Promise((resolve, reject) => {
-          resolve('butter');
-        })
-        const butter = await getButter;
-        console.log(`Husband: we got butter ${butter}`);
-        console.log(`Husband: Now we should go in`);
-  
 
+    const preMovie = async () => {
+      const wifeBringingTickets = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve('tickets');
+        }, 3000);
+      })
 
+      let ticket = await wifeBringingTickets;
 
-        return ticket;
-      }
+      console.log(`Wife: I have the tickets ${tickets}`);
+      console.log(`Husband: we should go in`);
+      console.log(`Wife: No i need popcorn`);
 
-      console.log(preMovie);
-      
+      const getPopCorn = new Promise((resolve, reject) => {
+        resolve('pocorn');
+      })
+      let popcorn = await getPopCorn;
+      console.log(`Husband: we got popcorn ${popcorn}`);
+      console.log(`Husband: Now we should go in`);
+      console.log(`Wife: No i need butter`);
+
+      const getButter = new Promise((resolve, reject) => {
+        resolve('butter');
+      })
+      const butter = await getButter;
+      console.log(`Husband: we got butter ${butter}`);
+      console.log(`Husband: Now we should go in`);
+
+      return ticket;
+    }
+    console.log(preMovie);
+
   }
 
   render() {
@@ -128,30 +124,60 @@ export default class App extends Component {
     this.promise()
     this.complexPromise()
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <View style={styles.viewStyleOne}>
+            <Text style={styles.textStyle}> 1 </Text>
+          </View>
+          <View style={styles.viewStyleTwo}>
+            <Text style={styles.textStyle}> 2 </Text>
+          </View>
+          <View style={styles.viewStyleThree}>
+            <Text style={styles.textStyle}> 3 </Text>
+          </View>
+        </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#4286f4',
     flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  viewStyleOne: {
+    height: 40,
+    width: 40,
+    flexGrow: 0.3,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue'
+  },
+  viewStyleTwo: {
+    width: 40,
+    height: 40,
+    flexGrow: 0.3,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'green'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  viewStyleThree: {
+    width: 40,
+    height: 40,
+    flexGrow: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'red'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  textStyle: {
+    textAlign: 'center'
   },
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#ddd'
+  }
 });
