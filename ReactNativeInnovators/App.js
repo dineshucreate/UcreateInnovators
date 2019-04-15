91bb84734f072701f9015e14774acd40ef5df6a7
Amit Agnihotri <amit@ucreate.co.in>
2019-04-15 18:01:55 +0530

    Login js move to container folder

--- ReactNativeInnovators/App.js
+++ ReactNativeInnovators/App.js
@@ -1,5 +1,5 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
-import HomeScreen from './src/HomeScreen';
+import HomeScreen from './src/container/login';
 import SettingScreen from './src/SettingScreen';
 
 const MainNavigator = createStackNavigator({
