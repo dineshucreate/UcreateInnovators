df254b6ea62fb326fd1b101d9c5246e5ad829983
shivam-ucreate <shivam@ucreate.co.in>
2019-04-17 12:31:53 +0530

    Flat list implementation

--- AwesomeProject/src/containers/home/index.js
+++ AwesomeProject/src/containers/home/index.js
@@ -1,15 +1,55 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList, Image, SafeAreaView } from 'react-native';
+import style from './style';
+import homeModel from '../../models/home';
 
 export default class home extends Component {
+	static navigationOptions = {
+		title: 'Home'
+	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			arrList: [ { empl: 'Alex', address: 'UK' } ]
+		};
+	}
+
+	componentDidMount() {
+		let home = new homeModel();
+		home.getEmployeeList(
+			(response) => {
+				this.setState({
+					arrList: response.data
+				});
+			},
+			(error) => {
+				alert('error');
+			}
+		);
+	}
+
+	renderListItem = ({ item }) => (
+		<View style={style.containerMain}>
+			<Image style={styles.userImage} />
+			<View style={styles.textView}>
+				<Text style={{ marginTop: 5 }}>{item.employee_name}</Text>
+				<Text style={{ marginTop: 5 }}>{item.employee_salary}</Text>
+			</View>
+		</View>
+	);
 	render() {
 		const { navigation } = this.props;
 		const user = navigation.getParam('user', 'no user');
-		console.log('Home output', user.email);
 		return (
-			<View>
-				<Text> textInComponent </Text>
-			</View>
+			<SafeAreaView>
+				<View>
+					<FlatList
+						//contentContainerStyle={styles.containerMain}
+						data={this.state.arrList}
+						renderItem={this.renderListItem}
+					/>
+				</View>
+			</SafeAreaView>
 		);
 	}
 }
