f3daa4874f9a0aea60948860b71cf99c358130d4
shivam-ucreate <shivam@ucreate.co.in>
2019-04-17 17:35:18 +0530

    Grid items in flat list

--- AwesomeProject/src/containers/home/index.js
+++ AwesomeProject/src/containers/home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Text, View, FlatList, Image, SafeAreaView } from 'react-native';
-import style from './style';
+import { Text, View, FlatList, Image, SafeAreaView, TouchableOpacity, ActivityIndicator } from 'react-native';
+import styles from './style';
 import homeModel from '../../models/home';
 
 export default class home extends Component {
@@ -10,44 +10,88 @@ export default class home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			arrList: [ { empl: 'Alex', address: 'UK' } ]
+			loading: false,
+			arrList: [],
+			error: null,
+			employeeObject: { name: 'Rohhiitt', age: '30', salary: '500000' }
 		};
 	}
 
 	componentDidMount() {
+		this.makeRemoteRequest();
+	}
+
+	makeRemoteRequest() {
 		let home = new homeModel();
+		this.setState({
+			loading: true
+		});
 		home.getEmployeeList(
 			(response) => {
 				this.setState({
-					arrList: response.data
+					arrList: response.data,
+					loading: false
 				});
 			},
 			(error) => {
-				alert('error');
+				this.setState({
+					error: error,
+					loading: false
+				});
 			}
 		);
 	}
 
+	onPress = (item) => {
+		alert(`Item pressed + ${item.employee_name}`);
+	};
+
+	_keyExtractor = (item, index) => item.id;
+
 	renderListItem = ({ item }) => (
-		<View style={style.containerMain}>
-			<Image style={styles.userImage} />
-			<View style={styles.textView}>
-				<Text style={{ marginTop: 5 }}>{item.employee_name}</Text>
-				<Text style={{ marginTop: 5 }}>{item.employee_salary}</Text>
+		<TouchableOpacity onPress={() => this.onPress(item)}>
+			<View style={styles.containerMain}>
+				<Image
+					style={styles.userImage}
+					source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }}
+				/>
+				<View style={styles.textView}>
+					<Text style={{ marginTop: 5 }}>{item.employee_name}</Text>
+					<Text style={{ marginTop: 5 }}>{item.employee_salary}</Text>
+				</View>
 			</View>
-		</View>
+		</TouchableOpacity>
 	);
+
+	renderSeparator = () => {
+		return <View style={styles.itemSeparator} />;
+	};
+
 	render() {
 		const { navigation } = this.props;
 		const user = navigation.getParam('user', 'no user');
 		return (
-			<SafeAreaView>
-				<View>
+			<SafeAreaView style={{ flex: 1 }}>
+				<View style={{ flex: 1, justifyContent: 'center' }}>
 					<FlatList
-						//contentContainerStyle={styles.containerMain}
 						data={this.state.arrList}
 						renderItem={this.renderListItem}
+						keyExtractor={this._keyExtractor}
+						ItemSeparatorComponent={this.renderSeparator}
+						numColumns={2}
 					/>
+					{this.state.loading && (
+						<ActivityIndicator
+							style={{
+								top: 0,
+								bottom: 0,
+								left: 0,
+								right: 0,
+								position: 'absolute'
+							}}
+							size="large"
+						/>
+					)}
 				</View>
 			</SafeAreaView>
 		);
--- AwesomeProject/src/containers/home/style.js
+++ AwesomeProject/src/containers/home/style.js
@@ -1,22 +1,43 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Dimensions } from 'react-native';
+const width = Dimensions.get('window').width;
 export default (styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
 	containerMain: {
 		flex: 1,
 		flexDirection: 'row',
-		height: 60
+		height: 80,
+		width: width / 2 - 20,
+		margin: 5
 	},
 	userImage: {
 		width: 50,
 		height: 50,
 		marginLeft: 10,
-		marginTop: 10,
+		marginTop: 5,
 		backgroundColor: 'gray',
 		borderRadius: 25
 	},
 	textView: {
+		flex: 1,
 		height: 50,
-		marginTop: 10,
-		marginLeft: 10
+		marginTop: 5,
+		marginLeft: 10,
+		marginRight: 10
+	},
+	itemSeparator: {
+		height: 1,
+		width: '86%',
+		backgroundColor: '#CED0CE',
+		marginLeft: '14%'
+	},
+	activityIndicator: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 80
 	}
 }));
