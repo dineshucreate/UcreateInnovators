fa282200037d27ed50d0a3b715989ff0d8d8e8d6
Nitish Devgan <nitish@ucreate.co.in>
2019-04-19 18:59:33 +0530

    Set dummy image in view

--- BasicsDemos/src/grid_view/GridViewScreen.js
+++ BasicsDemos/src/grid_view/GridViewScreen.js
@@ -1,23 +1,32 @@
 
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, Alert,TouchableOpacity} from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
-const data = [{key:'A'}, {key:'B'},{key:'c'}, {key:'d'},{key:'e'}, {key:'f'},{key:'g'}, {key:'h'},{key:'i'}, {key:'j'},
+const data = [{key:'Amit'}, {key:'Bobby'},{key:'Priya'}, {key:'Aman'},{key:'Karan'}, {key:'Mandeep'},{key:'Modi'}, {key:'Sunil'},{key:'Pawani'}, {key:'j'},
 ]
 export default class GridViewScreen extends React.Component{
 
+    actionOnRow(item){
+        console.log("Select"+ item)
+          }
     render(){
        return(
            <View style={styles.container}>
-           <FlatList
+           <FlatList 
            data= {data}
-           renderItem= {({item})=> <View style={styles.rowView}>
+           renderItem= {({item})=>
+         //  <TouchableOpacity onPress = {()=> this.actionOnRow(item)}> 
+           <View style={styles.rowView}>
+           <TouchableOpacity onPress={()=> this.actionOnRow(item)} >
            <Image source={require('../assets/placeholder_sticker.jpg')} style={styles.imageView}></Image>
                <Text style = {styles.textView}>
                      {item.key}
                </Text>
-       </View>}
+               </TouchableOpacity>
+       </View>
+     //  </TouchableOpacity>
+    }
         numColumns = {3}>
       
            </FlatList>
@@ -34,25 +43,28 @@ const styles = StyleSheet.create({
         margin:2,
     },
     textView:{
-        color:'#890000',
-        fontSize:29,
+        color:'#000',
+        fontSize:22,
+        marginBottom:2,
       
        
         // textAlignVertical:'center',
-        // textAlign:'center',
+         textAlign:'center',
     },
     rowView:{
         flex:1,
        // justifyContent:'center',
         alignItems:'center',
-        height:150,
-        margin:7,
-        backgroundColor: '#00BCD4'
+        height:170,
+        margin:3,
+        backgroundColor: '#fff',
+        elevation:2,
     },
     imageView:{
         flex:1,
        //width:50,
         height:130,
+        marginTop:4,
        
     }
 
