f5d9b795bd1b3cb90c9be26d75fb9d7135b85793
Nitish Devgan <nitish@ucreate.co.in>
2019-04-23 09:53:39 +0530

    set Custom icon on bottom bar

--- BasicsDemos/src/Bottom/BottomScreen.js
+++ BasicsDemos/src/Bottom/BottomScreen.js
@@ -6,6 +6,30 @@ import {createBottomTapNavigator} from 'react-navigation';
 const TabNavigator = createBottomTabNavigator({
     Home: { screen: HomeScreen },
     Settings: { screen: SettingsScreen },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+          // Sometimes we want to add badges to some icons. 
+          // You can check the implementation below.
+          IconComponent = HomeIconWithBadge; 
+        } else if (routeName === 'Settings') {
+          iconName = `ios-options`;
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
   });
   
   export default (TabNavigator);
