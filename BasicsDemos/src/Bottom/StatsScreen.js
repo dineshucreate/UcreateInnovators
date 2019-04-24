327c333608eb9336be501fce12877f68d1b69601
Nitish Devgan <nitish@ucreate.co.in>
2019-04-24 12:32:13 +0530

    Implement Tab view controller

--- BasicsDemos/src/Bottom/StatsScreen.js
+++ BasicsDemos/src/Bottom/StatsScreen.js
@@ -1,13 +1,51 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import {createStackNavigator, createMaterialTopTabNavigator, createAppContainer} from 'react-navigation';
+import FixtureScreen from '../tabs/FixtureScreen';
+import LatestScreen from '../tabs/LatestScreen';
+import TableScreen from '../tabs/TableScreen';
 
-
-export default class StatsScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>StatsScreen!</Text>
-        </View>
-      );
+const TabScreen = createMaterialTopTabNavigator({
+  fixture: {screen: FixtureScreen},
+  latest: {screen: LatestScreen},
+  table : {screen: TableScreen},
+},
+  {
+    tabBarOptions:'center',
+    swipeEnabled: true,
+    animationEnabled: true,
+    tabBarOptions:{
+      activeTintColor: '#AEE675',
+      inactiveTintColor: '#fff',
+      style:{
+        backgroundColor:'#096B76'
+      }
     }
   }
+)
+
+const StatsScreen = createStackNavigator({
+  TabScreen: {
+    screen: TabScreen,
+    navigationOptions:{
+      headerStyle:{
+        backgroundColor:'#633689',
+      },
+      headerTintColor: '#FFFFFF',
+      title:'TABExample'
+    },
+  },
+});
+
+
+//  class StatsScreen extends React.Component {
+//     render() {
+//       return (
+//         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//           <Text>StatsScreen!</Text>
+//         </View>
+//       );
+//     }
+//   }
+
+  export default createAppContainer(StatsScreen)
