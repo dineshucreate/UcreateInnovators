85c34ee366dfd5f0fb2a0af4cb7b895ed747cffb
Jaspreet Singh <jaspreet@ucreate.co.in>
2019-04-25 18:52:29 +0530

    Implementation of splash screen and handle navigation on that.

--- IMFApp/App.js
+++ IMFApp/App.js
@@ -3,6 +3,7 @@ import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
 import { AsyncStorage } from 'react-native'
 import stack from './src/navigationStack';
 import stack2 from './src/navigationStack2';
+import SplashScreen from 'react-native-splash-screen'
 
 export default class NavigationComponentInitial extends React.Component {
   constructor() {
@@ -21,7 +22,11 @@ export default class NavigationComponentInitial extends React.Component {
 
     }
   }
-
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
   render() {
     return (this.state.value === '' ?
       (<Begin1 />) : (<Begin2 />)
