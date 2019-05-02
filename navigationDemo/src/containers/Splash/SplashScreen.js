6180d3e6a943dc870c8a262c42a8fd0ef9ca556c
Karan Nassa <karannassa44@gmail.com>
2019-05-02 17:51:12 +0530

    Create Splash Screen

--- navigationDemo/src/containers/Splash/SplashScreen.js
+++ navigationDemo/src/containers/Splash/SplashScreen.js
@@ -2,13 +2,65 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 
+class Splash extends React.Component {
+    render() {
+        const viewStyles = [
+            SplashStyle.container,
+            { backgroundColor: 'orange' }
+        ];
+        const textStyles = {
+            color: 'white',
+            fontSize: 40,
+            fontWeight: 'bold'
+        };
+
+        return (
+            <View style={SplashStyle.container}>
+                <Text onPress={this.loadHome} style={{ fontSize: 40 }}>SPLASH SCREEN</Text>
+            </View>
+        );
+    }
+}
+
 export default class SplashScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { isLoading: true }
+    }
+
+    performTimeConsumingTask = async () => {
+        return new Promise((resolve) =>
+            setTimeout(
+                () => {
+                    resolve('result')
+                    this.props.navigation.navigate('Home');
+                },
+                2000
+            )
+        );
+    }
+
+    async componentDidMount() {
+        // Preload data from an external API
+        // Preload data using AsyncStorage
+        const data = await this.performTimeConsumingTask();
+
+        if (data !== null) {
+            this.setState({ isLoading: false });
+        }
+    }
 
     loadHome = () => {
         this.props.navigation.navigate('Home');
     }
 
     render() {
+        if (this.state.isLoading) {
+            return <Splash />;
+        }
+
         return (
             <View style={SplashStyle.container}>
                 <Text onPress={this.loadHome} style={{ fontSize: 40 }}>SPLASH SCREEN</Text>
@@ -23,5 +75,15 @@ const SplashStyle = StyleSheet.create({
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
 })
