064f903fbeb89d0e5e521ea0042747e7b0f2390b
Jaspreet Singh <jaspreet@ucreate.co.in>
2019-05-07 13:10:53 +0530

    Image picker implementation.

--- IMFApp/android/app/src/main/java/com/imfapp/MainApplication.java
+++ IMFApp/android/app/src/main/java/com/imfapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.imfapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new FBSDKPackage(),
             new SplashScreenReactPackage(),
             new RNGestureHandlerPackage()
--- IMFApp/src/containers/home/index.js
+++ IMFApp/src/containers/home/index.js
@@ -4,13 +4,47 @@ import styles from './style'
 import BackgroundImage from '../../component/backgroundImage'
 import { SafeAreaView } from 'react-navigation';
 import Modal from "react-native-modal";
+import ImagePicker from 'react-native-image-picker';
+
+const options = {
+    title: 'Select Avatar',
+    // customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+    storageOptions: {
+        skipBackup: true,
+        path: 'images',
+    },
+};
 
 export default class Home extends Component {
+
     state = {
-        isModalVisible: true
+        isModalVisible: true,
+        avatarSource: ''
     };
     toggleModal = () => {
         this.setState({ isModalVisible: !this.state.isModalVisible });
+    };
+    openCamera = () => {
+        ImagePicker.showImagePicker(options, (response) => {
+            console.log('Response = ', response);
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                const source = { uri: response.uri };
+
+                // You can also display the image using data:
+                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+
+                this.setState({
+                    avatarSource: source,
+                });
+            }
+        });
+
     };
     render() {
         return (
@@ -23,8 +57,12 @@ export default class Home extends Component {
                             <Image style={styles.facebookIcon} source={require('../../assets/images/stats-tabbar.png')} />
                         </TouchableOpacity>
                         <Text style={styles.headerText}>Home</Text>
-                        <Text style={styles.blankText}>aa</Text>
+                        <TouchableOpacity style={styles.menuButton} onPress={this.openCamera}>
+                            <Image style={styles.facebookIcon} source={require('../../assets/images/camera_icon.png')} />
+                        </TouchableOpacity>
                     </View>
+                    <Image style={{ height: 200, width: 200, marginTop: 50, alignSelf: 'center' }}
+                        source={this.state.avatarSource} />
                 </BackgroundImage>
                 <Modal isVisible={this.state.isModalVisible}>
                     <View style={styles.modalView}>
