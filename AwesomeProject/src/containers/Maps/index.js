7d98b4ef8722f39c65bad0975575ad3de9dceabd
shivam-ucreate <shivam@ucreate.co.in>
2019-05-09 13:33:06 +0530

    Place marker with coordinates

--- AwesomeProject/src/containers/Maps/index.js
+++ AwesomeProject/src/containers/Maps/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { View, SafeAreaView } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import styles from './style';
 import HeaderView from '../../components/HeaderView/Header';
 
 export default class Maps extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			markers: [
+				{
+					latitude: 30.7046,
+					longitude: 76.7179,
+					title: 'Foo Place',
+					subtitle: '1234 Foo Drive'
+				}
+			]
+		};
 	}
 
 	drawerOpen = () => {
@@ -19,7 +28,24 @@ export default class Maps extends Component {
 			<SafeAreaView style={{ flex: 1 }}>
 				<HeaderView headerTitle={'Maps'} onClickIcon={this.drawerOpen} />
 				<View style={styles.container}>
-					<MapView provider={PROVIDER_GOOGLE} style={styles.map} />
+					<MapView
+						provider={PROVIDER_GOOGLE}
+						style={styles.map}
+						region={{
+							latitude: 30.7046,
+							longitude: 76.7179,
+							latitudeDelta: 0.015,
+							longitudeDelta: 0.0121
+						}}
+					>
+						{this.state.markers.map((marker) => (
+							<Marker
+								coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+								title={marker.title}
+								description={marker.description}
+							/>
+						))}
+					</MapView>
 				</View>
 			</SafeAreaView>
 		);
