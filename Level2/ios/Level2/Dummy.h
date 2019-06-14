0036bdf796679cbc8d0f63f21fb2d5e5f7a32a3e
Arvinder singh <arvinder@ucreate.co.in>
2019-06-14 12:33:23 +0530

    Practicing native modules.

---
+++ Level2/ios/Level2/Dummy.h
@@ -0,0 +1,17 @@
+//
+//  Dummy.h
+//  Level2
+//
+//  Created by Mac on 10/06/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <React/RCTBridgeModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dummy : NSObject <RCTBridgeModule>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Level2/ios/Level2/Dummy.m
@@ -0,0 +1,13 @@
+//
+//  Dummy.m
+//  Level2
+//
+//  Created by Mac on 10/06/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import "Dummy.h"
+
+@implementation Dummy
+
+@end
